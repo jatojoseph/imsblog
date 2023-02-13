2631aac0776f87242a8776b5a3c880546a40a202
Jato Joseph <jaotjoseph4@gmail.com>
2023-02-13 12:15:48 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -84,13 +84,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
- '''DATABASES = {
+ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
+        'NAME': BASE_DIR / 'db.sqlite3',   }}
+
 
 
 
